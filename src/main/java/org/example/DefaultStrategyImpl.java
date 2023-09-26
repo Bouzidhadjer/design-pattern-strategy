5a36fcfce71cc3f49f53993cf6029fce930415bf
hadjerbzd <bouzidhadjer34@gmail.com>
2023-09-26 20:15:36 +0100

    add projet to github

---
+++ src/main/java/org/example/DefaultStrategyImpl.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class DefaultStrategyImpl implements Strategy{
+    @Override
+    public void operationStrategy() {
+        System.out.println("*******************");
+        System.out.println("======default Strategy ===========");
+        System.out.println("===================");
+    }
+}
---
+++ src/main/java/org/example/StrategyImpl1.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class StrategyImpl1  implements  Strategy {
+
+
+    @Override
+    public void operationStrategy() {
+        System.out.println("+++++++++++Strategy 1++++++++");
+    }
+}
